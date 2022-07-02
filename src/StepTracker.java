0fc2e69588d5254939de0e251e76e9c8086ce07a
Ilin Igor <zushka@yandex.ru>
2022-07-02 20:22:24 +0300

    Реализована логика  сохранения данных пользователя и вывод на экран.

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,5 +1,4 @@
 public class StepTracker {
-
     MonthData[] monthToData;
 
     static int goal = 10000;
@@ -12,27 +11,34 @@ public class StepTracker {
         }
     }
 
-    class MonthData { // 30 дней в месяце
-        int[] stepsOnMonth = new int[30];
-        int steps = 0; // Всего шагов
+    public void saveStatistics(int month, int day, int steps) {
+        monthToData[month].monthDays[day-1] = steps;
+    }
+
+    public void getInfo(int month) {
+        int[] monthDays = monthToData[month].monthDays;
+        for (int i = 0; i < monthDays.length; i++) {
+            System.out.print(i+1 + " день: " + monthDays[i] + ", ");
+        }
+    }
+
+    static class MonthData { // 30 дней в месяце
+        int[] monthDays = new int[30];
+
+         // Всего шагов
         int maxCountSteps = 0; // Максимальное колличество шагов в месяц
         double averageCountSteps = 0.0; // Среднее колличество шагов в месяц
-        int countDayBestSteps= 0; // Лучшая серия колличества шагов в месяц
+        int countDayBestSteps = 0; // Лучшая серия колличества шагов в месяц
         int series = 0; // Серия
-        public int[] getStepsOnMonth() { // шаги в месяц
-            return stepsOnMonth;
-        }
-    }
-
-        void saveStatistics(int month, int day, int steps) {
 
         }
 
-        int goalSteps(int userGoal){ // Замена цели
+
+    public void goalSteps(int userGoal) { // Замена цели
         StepTracker.goal = userGoal;
-        return userGoal;
-        }
     }
+}
+
 
 
 
